60816874d45c90fd96b6e93ff97476c3ae423f0c
Scott Tunstall <scott.tunstall@ntlworld.com>
2021-12-29 18:40:50 +0000

    Initial checkin for safekeeping.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using LeetcodeFirstMissingPositive;
+
+var solution = new Solution();
+
+var nums = new int[] {-1, 4, 2, 5, 9, 10, 0};
+var answer = solution.FirstMissingPositive(nums);
+
+Console.WriteLine(answer);
---
+++ Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeFirstMissingPositive
+{
+    public class Solution
+    {
+        public int FirstMissingPositive(int[] nums)
+        {
+            for (int i=0; i < nums.Length;)
+            {
+                var num = nums[i];
+
+                Console.WriteLine($"Read {num} at index {i}");
+
+                var toZeroBasedIndex = num - 1;
+
+                if (num > 0 && num < nums.Length && num != nums[toZeroBasedIndex])
+                {
+                    int temp = nums[toZeroBasedIndex];
+                    nums[toZeroBasedIndex] = num;
+                    nums[i] = temp;
+
+                    Console.WriteLine($"swapping {num} with {temp}");
+                }
+                else
+                    i++;
+            }
+            
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != i + 1)
+                    return i + 1;
+            }
+
+            return nums[nums.Length - 1] + 1;
+        }
+    }
+}
